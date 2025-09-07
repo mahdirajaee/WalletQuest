f79557372d3308a5d90f7232ae27096451d22a9f
Mahdi <mahdi.rajaee@ymail.com>
2025-09-07 17:57:41 +0200

    Initial Commit

---
+++ WalletQuest/WalletQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletQuestApp.swift
+//  WalletQuest
+//
+//  Created by mahdi on 07/09/25.
+//
+
+import SwiftUI
+
+@main
+struct WalletQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
